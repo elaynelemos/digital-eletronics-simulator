042ac688af295a67d2882e57668102d62049011f
Elayne Lemos <elayne.l.lemos@gmail.com>
2020-01-14 16:48:46 -0300

    Exception handling of Logic Analyzer and adjusting cases

--- logicanalyzer.py
+++ logicanalyzer.py
@@ -25,16 +25,35 @@ class LogicAnalyzer:
         self.__checkers = checkers
     
 
-    def defineValues() -> None:
-        for i in self.__checkers:
-            if i.getValue() is None and i.getChecked() is False:
+    def defineValues(self) -> bool:
+        try:
+            for i in self.__entries:
+                for j in self.__entries:
+                    if isEqualPoints(i.getCoords(), j.getCoords()):
+                        raise RuntimeError("Error! Entries connected:", i.id(), j.id())
                 for j in self.__wires:
                     if isEqualPoints(i.getCoords(), j.getWireEndP()):
                         for k in self.__entries:
                             if isEqualPoints(j.getWireStartP(), k.getCoords()):
-                                i.setValue(k.getValue())
-            i.setChecked(True)
+                                raise RuntimeError("Error! Entries connected:", i.id(), k.id())
+        except RuntimeError as re:
+            print(re)
+            return False
+        else:
+            for i in self.__checkers:
+                if i.getValue() is None and i.getChecked() is False:
+                    for j in self.__entries:
+                        if isEqualPoints(i.getCoords(), j.getCoords()):
+                            i.setValue(j.getValue())
+                    for j in self.__wires:
+                        if isEqualPoints(i.getCoords(), j.getWireEndP()):
+                            for k in self.__entries:
+                                if isEqualPoints(j.getWireStartP(), k.getCoords()):
+                                    i.setValue(k.getValue())
+                i.setChecked(True)
+            return True
 
-    def prepareCheckers(checkers:List[Checker]) -> None:
-        for i in checkers:
+    def prepareCheckers(self) -> None:
+        for i in self.__checkers:
             i.setChecked(False)
+
