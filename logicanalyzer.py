d0acdf15f010ff69437f0116cc8172607a676841
Elayne Lemos <elayne.l.lemos@gmail.com>
2020-01-14 09:56:54 -0300

    Initial structure and limited verification function.

---
+++ logicanalyzer.py
@@ -0,0 +1,42 @@
+"""
+*   Universidade Federal do Vale do São Francisco - Univasf
+*   Colegiado de Engenharia de Computação
+*   Orientador: Prof. Dr. Jorge Cavalcanti
+*   Discentes: Elayne Lemos, elayne.l.lemos@gmail.com
+*              Jônatas de Castro, jonatascastropassos@gmail.com
+*   Atividade: pt-br/ este arquivo define as constantes a serem utilizadas pelo simulador de 
+*                     eletrônica digital.
+*              en-us/ this file defines the constants those will be used at the digital
+*                     eletronics simulator.
+*
+"""
+
+from typing import List
+from components import *
+
+class LogicAnalyzer:
+    __entries:List[Entry] = []
+    __wires:List[Wire] = []
+    __checkers:List[Checker] = []
+
+    def __init__(self, entries:List[Entry], wires:List[Wire],checkers:List[Checker]) -> None:
+        self.__entries = entries
+        self.__wires = wires
+        self.__checkers = checkers
+        
+        
+def factCheck(entries:List[Entry], wires:List[Wire],checkers:List[Checker]) -> None:
+    for i in checkers:
+        if i.getValue() is None and i.getChecked() is False:
+            for j in wires:
+                if isEqualPoints(i.getCoords(), j.getWireEndP()):
+                    for k in entries:
+                        if isEqualPoints(j.getWireStartP(), k.getCoords()):
+                            i.setValue(k.getValue())
+            i.setChecked(True)
+            if not(i.getChecked()):
+                i.setValue(None)
+
+def prepareCheckers(checkers:List[Checker]) -> None:
+    for i in checkers:
+        i.setChecked(False)
